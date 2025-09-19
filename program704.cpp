14377753597314fb86e92ab01c67ffbb2a19600a
rohan-9513 <rohan@gmail.com>
2025-09-20 02:09:14 +0530

    my first commit

---
+++ program704.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Jay Ganesh...\n";
+
+    return 0;
+}
+
+
